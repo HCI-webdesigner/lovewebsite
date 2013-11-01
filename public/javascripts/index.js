dcd87c6ec5c2843c7080893d86d6c2bfcc5617a7
xing240 <787923046@qq.com>
2013-11-01 15:22:13 +0800

    change index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,7 @@
-window.onload = function(){
-	var speed = 10;
-	var tab = document.getElementById("love-persons-box");
-	var tab1 = document.getElementById("love-persons-1");
-	var tab2 = document.getElementById("love-persons-2");
-	tab2.innerHTML=tab1.innerHTML;
-	function Marquee(){
-		if(tab2.offsetWidth<=tab.scrollLeft)
-			tab.scrollLeft-=tab1.offsetWidth;
-		else tab.scrollLeft++;
-	}
-	var mar = setInterval(Marquee,speed);
-	tab.onmouseover=function(){clearInterval(mar)};
-	tab.onmouseout=function(){mar = setInterval(Marquee,speed)};
+window.onload = function() {
+	var newDom = document.getElementById("new-info");
+	var inter=setInterval(function() {
+		var fromFirstChild = newDom.removeChild(newDom.firstChild);
+		newDom.appendChild(fromFirstChild);
+	}, 1000);
 }
