b45f8a9ccc7ac486ded012e55665362270c1f341
xing240 <787923046@qq.com>
2013-11-02 22:55:44 +0800

    fix index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,16 @@
 window.onload = function() {
-	var newDom = document.getElementById("new-info");
-	var inter=setInterval(function() {
-		var fromFirstChild = newDom.removeChild(newDom.firstChild);
-		newDom.appendChild(fromFirstChild);
-	}, 1500);
+	var infoDom = document.getElementById("new-info");
+	var listDom = document.getElementById("new-list");
+	var inter = setInterval(function() {
+		var move = setInterval(function() {
+			if (infoDom.scrollTop < 30) {
+				infoDom.scrollTop += 3;
+			} else {
+				firstChildDom=listDom.getElementsByTagName("li")[0];
+				listDom.appendChild(listDom.removeChild(firstChildDom));
+				infoDom.scrollTop = 0;
+				clearInterval(move);
+			}
+		}, 100);
+	}, 3000);
 }
